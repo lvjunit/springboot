700184896ffb275773a8a65fa37ee44ad803d196
lvjun <lvjun@guahao.com>
2018-09-30 10:25:03 +0800

    springboot简单操作crud

---
+++ springboot/src/main/java/com/how2java/springboot/pojo/Category.java
@@ -0,0 +1,26 @@
+package com.how2java.springboot.pojo;
+
+/**
+ * Created by lvjun on 2018-09-30.
+ */
+public class Category {
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
